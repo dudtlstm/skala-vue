153efaf55da247261f570ff8219c8f1e141fbc31
dudtlstm <annpys1024@naver.com>
2026-08-26 15:55:04 +0900

    chore: App.vue에 ch04/ch05 컴포넌트 등록

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
 import WeatherMockup from './components/handson/WeatherMockup.vue'
-import PropsEmitsParent from './components/practices/component/PropsEmitsParent.vue'
+import WeatherComposition from './components/handson/WeatherComposition.vue'
+import WeatherParent from './components/handson/WeatherParent.vue'
 </script>
 
 <template>
   <header>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
-    <div class="wrapper">
+    <div class="app-container">
       <WeatherMockup />
-      <PropsEmitsParent />
+      <WeatherComposition />
+      <WeatherParent />
       <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
